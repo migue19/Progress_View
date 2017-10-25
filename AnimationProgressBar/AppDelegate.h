c48ea18ea37782816c282a75466a6063198a9870
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2017-10-25 17:33:38 -0500

    Initial Commit

---
+++ AnimationProgressBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationProgressBar
+//
+//  Created by Miguel Mexicano on 25/10/17.
+//  Copyright © 2017 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationProgressBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationProgressBar
+//
+//  Created by Miguel Mexicano on 25/10/17.
+//  Copyright © 2017 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
