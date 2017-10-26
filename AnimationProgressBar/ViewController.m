4d50d90ec31b7c2a6b51a673cacd38890c73b53e
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2017-10-25 22:59:19 -0500

    add all project

--- AnimationProgressBar/ViewController.m
+++ AnimationProgressBar/ViewController.m
@@ -13,11 +13,36 @@
 @end
 
 @implementation ViewController
+{
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.progressView.progress = 0.0;
+    
+    //[[self.progressView layer] cornerRadius] = 20 ;
+    [self performSelectorOnMainThread:@selector(makeMyProgressBarMoving) withObject:nil waitUntilDone:NO];
+
 }
+
+
+
+- (void)makeMyProgressBarMoving {
+    
+    float actual = [self.progressView progress];
+    if (actual < 1) {
+        self.progressView.progress = actual + 0.1;
+        [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(makeMyProgressBarMoving) userInfo:nil repeats:NO];
+    }
+    else{
+        
+        
+        
+        
+    }
+    
+} 
 
 
 - (void)didReceiveMemoryWarning {
